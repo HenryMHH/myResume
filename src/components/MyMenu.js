7e22313b0474626c5df7258de49e7c16b6112e69
HenryHo <minghang.h@gmail.com>
2022-02-13 19:42:13 +0800

    tune header

--- src/components/MyMenu.js
+++ src/components/MyMenu.js
@@ -1,5 +1,5 @@
 import { Box, List, ListItem, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react'
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { modalOpenContext } from '../context/modalOpenContext'
 import { profolioList } from '../projectList'
@@ -19,6 +19,7 @@ const StyledItem = styled(MenuItem)`
 
 export default function MyMenu() {
 	const { setModalState, setDrawerState } = useContext(modalOpenContext)
+	const [isScroll, setIsScroll] = useState(false)
 
 	function triggerModal(index, type) {
 		setModalState((pre) => (pre = { index: index, type: type }))
@@ -32,11 +33,36 @@ export default function MyMenu() {
 			widthWindow = window.innerWidth
 		}
 		window.addEventListener('resize', widthSetter)
-		return () => widthSetter()
+		return () => window.removeEventListener('resize', widthSetter)
+	}, [])
+
+	useEffect(() => {
+		window.addEventListener('scroll', scrollHandler)
+		function scrollHandler() {
+			if (window.scrollY > 50) {
+				setIsScroll(true)
+			} else {
+				setIsScroll(false)
+			}
+		}
+		return () => window.removeEventListener('scroll', scrollHandler)
 	}, [])
 
 	return (
-		<Box zIndex="5" w="100%" alignItems="center" pos="fixed" display="flex" justifyContent="space-between" fontSize="1.5em" p=".3em 1em" top="0" transition="all .3s ease">
+		<Box
+			bg={isScroll ? 'rgba(255,255,255,1)' : 'transparent'}
+			zIndex="5"
+			w="100%"
+			alignItems="center"
+			pos="fixed"
+			display="flex"
+			justifyContent="space-between"
+			fontSize="1.5em"
+			p=".3em 1em"
+			top="0"
+			transition="all .3s ease"
+			boxShadow={isScroll ? '2px 9px 17px -3px #000000' : 'none'}
+		>
 			<FakeIcon />
 			{widthWindow <= 768 ? (
 				<Box
